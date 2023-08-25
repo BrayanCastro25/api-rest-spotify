ec1c3bdce6d1ffcef3bbb4e5ddedd8334ecac0e8
Brayan Castro <davidbarbosa0525@gmail.com>
2023-08-24 21:05:47 -0500

    validation on user registration data

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,6 @@
+// Importación de helpers
+const validate = require("../helpers/validate");
+
 const prueba = (req, res) => {
     return res.status(200).json({
         status: "success",
@@ -5,6 +8,47 @@ const prueba = (req, res) => {
     })
 };
 
+const register = (req, res) => {
+    // Recoger los datos que llegan de la petición
+    let params = req.body;
+
+    // Comprobar que llegan bien
+    if(!params.name || !params.nick || !params.email || !params.password){
+        return res.status(400).json({
+            status: "error",
+            message: "Faltan datos por enviar"
+        });
+    }
+
+    // Validar los datos
+    try{
+        validate(params);        
+    }catch(error){
+        return res.status(400).json({
+            status: "error",
+            message: "Validación no superada"
+        });
+    }
+
+    // Control de usuarios duplicados
+
+    // Cifrar la contraseña
+
+    // Crear objeto del usuario
+
+    // Guardar usuario en la BD
+
+    // Limpiar el objeto a devolver
+
+    // Devolver un resultado
+
+    return res.status(200).json({
+        status: "success",
+        message: "Método de registro"
+    })
+};
+
 module.exports = {
-    prueba
+    prueba,
+    register
 }
