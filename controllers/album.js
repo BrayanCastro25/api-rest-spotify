4e4b254e7b95f6b4ef648008691884f32adfe9cc
Brayan Castro <davidbarbosa0525@gmail.com>
2023-09-01 19:31:36 -0500

    creation method save album

--- controllers/album.js
+++ controllers/album.js
@@ -1,3 +1,6 @@
+// Importación de modelo
+const Album = require("../models/album");
+
 const prueba = (req, res) => {
     return res.status(200).json({
         status: "success",
@@ -5,6 +8,33 @@ const prueba = (req, res) => {
     })
 };
 
+const save = (req, res) => {
+
+    // Sacar datos del body
+    let params = req.body;
+
+    // Crear objeto
+    let album = new Album(params);
+
+    // Guardar el objeto
+    album.save()
+        .then((albumStored) => {
+            return res.status(200).json({
+                status: "success",
+                message: "Metodo guardar album",
+                albumStored
+            });
+        })
+        .catch((error) => {
+            return res.status(400).json({
+                status: "error",
+                message: "Error al guardar el album"
+            });
+        });
+
+}
+
 module.exports = {
-    prueba
+    prueba,
+    save
 }
--- routes/album.js
+++ routes/album.js
@@ -1,5 +1,6 @@
 // Importar dependencias
 const exprees = require("express");
+const check = require("../middlewares/auth");
 
 // Cargar Router
 const router = exprees.Router();
@@ -9,6 +10,7 @@ const AlbumController = require("../controllers/album");
 
 // Definir rutas
 router.get("/test", AlbumController.prueba);
+router.post("/save", check.auth, AlbumController.save)
 
 // Exportar router
 module.exports = router;
