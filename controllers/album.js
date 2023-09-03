3c7b0cad1351f6df06b243842c5fd7ad413fe076
Brayan Castro <davidbarbosa0525@gmail.com>
2023-09-02 20:57:11 -0500

    creation of methods to upload and show image of an album

--- controllers/album.js
+++ controllers/album.js
@@ -1,6 +1,9 @@
 // Importación de modelo
-const album = require("../models/album");
 const Album = require("../models/album");
+
+// Importar dependencias
+const fs = require("fs");
+const path = require("path");
 
 const prueba = (req, res) => {
     return res.status(200).json({
@@ -111,10 +114,88 @@ const update = (req, res) => {
         });
 };
 
+
+const upload = (req, res) => {
+
+    // Recoger album id
+    let albumId = req.params.id;
+
+    // Recoger fichero de imagen y comprobar si existe
+    if(!req.file){
+        return res.status(404).json({
+            status: "error",
+            message: "La petición no incluye la imagen",
+        })
+    }
+
+    // Conseguir el nombre de la imagen
+    let image = req.file.originalname;
+
+    // Sacar info de la imagen
+    const imageSplit = image.split("\.");
+    const extension = imageSplit[1];
+
+    // Comprobar si la extensión es válida
+    if(extension != "jpg" && extension != "png" && extension != "gif" && extension != "jpeg"){
+        // Borrar archivo
+        const filePath = req.file.path;
+        const fileDeleted = fs.unlinkSync(filePath);
+
+        // Devover error
+        return res.status(400).json({
+            status: "error",
+            message: "La extensión no es válida"
+        })
+    }
+
+    // Si es correcto, guardar la imagen en la BBDD
+    Album.findOneAndUpdate({_id: albumId}, {image: req.file.filename}, {new: true})
+        .then((albumUpdated) => {
+            return res.status(200).json({
+                status: "success",
+                album: albumUpdated,
+                file: req.file
+            });
+        })
+        .catch((error) => {
+            return res.status(200).json({
+                status: "error",
+                message: "Error en la subida de la imagen del artista"
+            });
+        })
+
+    
+};
+
+
+const image = (req, res) => {
+    // Sacar el parametro de la url
+    const file = req.params.file;
+
+    // Montar el path real de la imagen
+    const filePath = "./uploads/albums/" + file;
+
+    // Comprobar que existe el fichero
+    fs.stat(filePath, (error, exists) => {
+        if(error || !exists){
+            return res.status(404).json({
+                status: "error",
+                message: "No existe la imagen"
+            })
+        }
+
+        return res.sendFile(path.resolve(filePath))
+    })
+
+    // Devolve el fichero
+};
+
 module.exports = {
     prueba,
     save,
     one,
     list,
-    update
+    update,
+    upload,
+    image
 }
--- routes/album.js
+++ routes/album.js
@@ -8,12 +8,28 @@ const router = exprees.Router();
 // Importar controlador
 const AlbumController = require("../controllers/album");
 
+// Configuración de subida
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads/albums/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, "albums-"+Date.now()+"-"+file.originalname);
+    }
+});
+
+const uploads = multer({storage});
+
 // Definir rutas
 router.get("/test", AlbumController.prueba);
 router.post("/save", check.auth, AlbumController.save);
 router.get("/one/:id", check.auth, AlbumController.one);
 router.get("/list/:artistId", check.auth, AlbumController.list);
 router.put("/update/:albumId", check.auth, AlbumController.update);
+router.post("/upload/:id", [check.auth, uploads.single("file0")], AlbumController.upload);
+router.get("/image/:file", AlbumController.image);
 
 // Exportar router
 module.exports = router;
